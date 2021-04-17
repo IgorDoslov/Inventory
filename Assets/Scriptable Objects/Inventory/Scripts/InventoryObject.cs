62ed3b7e48baae0b36cc72558c571387cddf489f
Igor Doslov <s191719@students.aie.edu.au>
2021-04-17 23:41:07 +1000

    Start

---
+++ Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEditor;
+using System.Runtime.Serialization;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+public class InventoryObject : ScriptableObject
+{
+    public string savePath;
+    public ItemDatabaseObject database;
+    public Inventory Container;
+
+    public void AddItem(Item a_item, int a_amount)
+    {
+
+        for (int i = 0; i < Container.Items.Count; i++)
+        {
+            if (Container.Items[i].item == a_item)
+            {
+                Container.Items[i].AddAmount(a_amount);
+                return;
+            }
+        }
+
+        Container.Items.Add(new InventorySlot(a_item.Id, a_item, a_amount));
+
+    }
+
+    [ContextMenu("Save")]
+    public void Save()
+    {
+        //string saveData = JsonUtility.ToJson(this, true);
+        //BinaryFormatter bf = new BinaryFormatter();
+        //FileStream file = File.Create(string.Concat(Application.persistentDataPath, savePath));
+        //bf.Serialize(file, saveData);
+        //file.Close();
+
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream(string.Concat(Application.persistentDataPath, savePath), FileMode.Create, FileAccess.Write);
+        formatter.Serialize(stream, Container);
+        stream.Close();
+    }
+    [ContextMenu("Load")]
+    public void Load()
+    {
+        if (File.Exists(string.Concat(Application.persistentDataPath, savePath)))
+        {
+            //BinaryFormatter bf = new BinaryFormatter();
+            //FileStream file = File.Open(string.Concat(Application.persistentDataPath, savePath), FileMode.Open);
+            //JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
+            //file.Close();
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(string.Concat(Application.persistentDataPath, savePath), FileMode.Open, FileAccess.Read);
+            Container = (Inventory)formatter.Deserialize(stream);
+            stream.Close();
+        }
+    }
+    [ContextMenu("Clear")]
+    public void Clear()
+    {
+        Container = new Inventory();
+    }
+
+    
+}
+
+[System.Serializable]
+public class Inventory
+{
+    public List<InventorySlot> Items = new List<InventorySlot>();
+}
+
+[System.Serializable]
+public class InventorySlot
+{
+    public int ID;
+    public Item item;
+    public int amount;
+    public InventorySlot(int a_ID, Item a_item, int a_amount)
+    {
+        ID = a_ID;
+        item = a_item;
+        amount = a_amount;
+    }
+
+    public void AddAmount(int a_value)
+    {
+        amount += a_value;
+    }
+}
---
+++ Assets/Scriptable Objects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Food,
+    Equipment,
+    Default
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public int Id;
+    public Sprite uiDisplay;
+    public ItemType type;
+    [TextArea(15, 20)]
+    public string description;
+}
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int Id;
+    public Item(ItemObject item)
+    {
+        name = item.name;
+        Id = item.Id;
+    }
+}
